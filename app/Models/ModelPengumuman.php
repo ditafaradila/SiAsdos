c0d2c4f9456825e38990b5dddbfd0e53f702416d
nesadwi <83526578+nesadwi@users.noreply.github.com>
2022-10-10 21:50:32 +0700

    Add files via upload

--- app/Models/ModelPengumuman.php
+++ app/Models/ModelPengumuman.php
@@ -8,18 +8,22 @@ class ModelPengumuman extends Model
 {
     protected $table = "pengumuman";
     protected $primaryKey = "id";
-    protected $allowedFields = ['nama', 'npm', 'mk', 'semester', 'kelas'];
 
-    function cari($katakunci)
-    {
-        //budi gmail
-        $builder = $this->table("pengumuman");
-        $arr_katakunci = explode(" ", $katakunci);
-        for ($x = 0; $x < count($arr_katakunci); $x++) {
-            $builder->orLike('nama', $arr_katakunci[$x]);
-            $builder->orLike('npm', $arr_katakunci[$x]);
-            $builder->orLike('mk', $arr_katakunci[$x]);
-        }
-        return $builder;
-    }
+    protected $useTimestamps = true;
+    protected $allowedFields = ['nama', 'npm', 'mk', 'semester', 'kelas'];
+    protected $dateFormat    = 'datetime';
+    protected $createdField  = 'tanggal_masuk';
+    protected $updatedField  = 'update_at';
+    // function cari($katakunci)
+    // {
+    //     //budi gmail
+    //     $builder = $this->table("pengumuman");
+    //     $arr_katakunci = explode(" ", $katakunci);
+    //     for ($x = 0; $x < count($arr_katakunci); $x++) {
+    //         $builder->orLike('nama', $arr_katakunci[$x]);
+    //         $builder->orLike('npm', $arr_katakunci[$x]);
+    //         $builder->orLike('mk', $arr_katakunci[$x]);
+    //     }
+    //     return $builder;
+    // }
 }
