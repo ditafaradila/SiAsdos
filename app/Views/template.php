188f3579e0cf62b0ed238de0cc6b4cf463aaa926
sarahcantika <83532698+sarahcantika@users.noreply.github.com>
2022-10-10 22:05:12 +0700

    Update Template

--- app/Views/template.php
+++ app/Views/template.php
@@ -76,13 +76,13 @@
               </a>
               <ul class="nav nav-treeview">
                 <li class="nav-item">
-                  <a href="/pengumuman" class="nav-link">
+                  <a href="/pengumuman_view" class="nav-link">
                     <i class="far fa-circle nav-icon"></i>
                     <p>Pengumuman AsDos</p>
                   </a>
                 </li>
                 <li class="nav-item">
-                  <a href="pengumuman" class="nav-link">
+                  <a href="/pengumuman" class="nav-link">
                     <i class="far fa-circle nav-icon"></i>
                     <p>Pengumuman Lainnya</p>
                   </a>
