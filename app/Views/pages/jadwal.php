8bbe143941e7d04e25da5b0006036f173ad00780
sarahcantika <83532698+sarahcantika@users.noreply.github.com>
2022-10-10 22:06:22 +0700

    Update jadwal

--- app/Views/pages/jadwal.php
+++ app/Views/pages/jadwal.php
@@ -1,4 +1,4 @@
-<?= $this->extend('template')?>
+<?= $this->extend('template') ?>
 <?= $this->section('content') ?>
 
 <h1 align="center">Sistem Informasi Asisten Dosen</h1>
@@ -9,7 +9,7 @@
   <h6>Jurusan Ilmu Komputer Fakultas Matematika dan Ilmu Pengetahuan Alam</h6>
 </div>
 <div>
-  <a href="/create" type="button" class="btn btn-primary">Create</a>
+  <a href="/create" type="button" class="btn btn-primary"><i class="fa fa-plus"></i> Create</a>
   <table class="table table-striped">
     <thead>
       <tr>
@@ -25,29 +25,29 @@
     </thead>
     <tbody class="table-group-divider">
       <?php $no = 1;
-        foreach ($asdos as $asds) :
+      foreach ($asdos as $asds) :
       ?>
-      <tr>
-                <th scope="row"><?= $no++ ?></th>
-                <td><?= $asds['jam'] ?></td>
-                <td><?= $asds['senin'] ?></td>
-                <td><?= $asds['selasa'] ?></td>
-                <td><?= $asds['rabu'] ?></td>
-                <td><?= $asds['kamis'] ?></td>
-                <td><?= $asds['jumat'] ?></td>
-                <td>
-                    <div class="d-flex">
-                        <a href="/edit/<?= $asds['id'] ?>" type="button" class="btn btn-primary"><i class="fa fa-pen"></i>Edit</a>
-                        <form method="POST" action="/delete/<?= $asds['id'] ?>">
-                            <input type="hidden" name="_method" value="DELETE">
-                            <button type="submit" name="submit" class="btn btn-danger"><i class="fa fa-trash"></i>Delete</button>
-                        </form>
-                    </div>
-                </td>
-            </tr>
-          <?php
-            endforeach
-          ?>
+        <tr>
+          <th scope="row"><?= $no++ ?></th>
+          <td><?= $asds['jam'] ?></td>
+          <td><?= $asds['senin'] ?></td>
+          <td><?= $asds['selasa'] ?></td>
+          <td><?= $asds['rabu'] ?></td>
+          <td><?= $asds['kamis'] ?></td>
+          <td><?= $asds['jumat'] ?></td>
+          <td>
+            <div class="d-flex">
+              <a href="/edit/<?= $asds['id'] ?>" type="button" class="btn btn-warning"><i class="fa fa-pen"></i>Edit</a>
+              <form method="POST" action="/delete/<?= $asds['id'] ?>">
+                <input type="hidden" name="_method" value="DELETE">
+                <button type="submit" name="submit" class="btn btn-danger"><i class="fa fa-trash"></i>Delete</button>
+              </form>
+            </div>
+          </td>
+        </tr>
+      <?php
+      endforeach
+      ?>
     </tbody>
   </table>
 </div>
