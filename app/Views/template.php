64f997300d9d8a6352769a4224821ff8efc0481e
sarahcantika <83532698+sarahcantika@users.noreply.github.com>
2022-10-10 21:51:05 +0700

    tambah sidebar

--- app/Views/template.php
+++ app/Views/template.php
@@ -1,77 +1,108 @@
 <!DOCTYPE html>
-<!--
-This is a starter template page. Use this page to start your new project from
-scratch. This page gets rid of all links and provides the needed markup only.
--->
 <html lang="en">
 
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>SiAsdos</title>
+  <title>Sistem Informasi Asdos</title>
 
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-  <!-- Font Awesome Icons -->
+  <!-- Font Awesome -->
   <link rel="stylesheet" href="/assets/AdminLTE/plugins/fontawesome-free/css/all.min.css">
+  <!-- overlayScrollbars -->
+  <link rel="stylesheet" href="/assets/AdminLTE/plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
   <!-- Theme style -->
   <link rel="stylesheet" href="/assets/AdminLTE/dist/css/adminlte.min.css">
   <link rel="stylesheet" href="/assets/AdminLTE/plugins/summernote/summernote-bs4.min.css">
 </head>
 
-<body class="hold-transition layout-top-nav">
+<body class="hold-transition sidebar-mini layout-fixed">
+  <!-- Site wrapper -->
   <div class="wrapper">
     <!-- Navbar -->
-    <nav class="main-header navbar navbar-expand-md navbar-light navbar-white">
+    <nav class="main-header navbar navbar-expand navbar-white navbar-light bg-primary">
       <div class="container">
-        <a href="" class="navbar-brand">
-          <img src="/assets/AdminLTE/dist/img/unila.jpg" alt="Unila Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-          <span class="brand-text font-weight-light">SiAsdos</span>
-        </a>
-
-        <button class="navbar-toggler order-1" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div class="collapse navbar-collapse order-3" id="navbarCollapse">
-          <!-- Left navbar links -->
-          <ul class="navbar-nav">
-            <li class="nav-item">
-              <a href="/home" class="nav-link">Home</a>
-            </li>
-            <li class="nav-item">
-              <a href="/materi" class="nav-link">Materi</a>
-            </li>
-            <li class="nav-item">
-              <a href="/profile" class="nav-link">Profile</a>
-            </li>
-            <li class="nav-item">
-              <a href="/pengumuman_view" class="nav-link">Pengumuman</a>
-            </li>
-          </ul>
-
-          <!-- SEARCH FORM -->
-          <form class="form-inline ml-0 ml-md-3">
-            <div class="input-group input-group-sm">
-              <input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search">
-              <div class="input-group-append">
-                <button class="btn btn-navbar" type="submit">
-                  <i class="fas fa-search"></i>
-                </button>
-              </div>
-            </div>
-          </form>
-        </div>
+        <!-- Left navbar links -->
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+          </li>
+        </ul>
       </div>
     </nav>
     <!-- /.navbar -->
 
+    <!-- Main Sidebar Container -->
+    <aside class="main-sidebar sidebar-dark-primary elevation-4 bg-primary">
+      <!-- Brand Logo -->
+      <a href="/assets/AdminLTE/index3.html" class="brand-link">
+        <img src="/assets/AdminLTE/dist/img/person.jpg" alt="Person Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+        <span class="brand-text font-weight-light">Sistem Informasi</span>
+      </a>
+
+      <!-- Sidebar -->
+      <div class="sidebar">
+        <!-- Sidebar user (optional) -->
+        <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+          <div class="image">
+            <img src="/assets/AdminLTE/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+          </div>
+          <div class="info">
+            <a href="/profile" class="d-block">Admin</a>
+          </div>
+        </div>
+
+        <!-- Sidebar Menu -->
+        <nav class="mt-2">
+          <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+            <!-- Add icons to the links using the .nav-icon class
+               with font-awesome or any other icon font library -->
+            <li class="nav-item">
+              <a href="/jadwal" class="nav-link">
+                <i class="nav-icon fas fa-th"></i>
+                <p>
+                  Jadwal
+                </p>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a href="#" class="nav-link">
+                <i class="nav-icon fas fa-tachometer-alt"></i>
+                <p>
+                  Pengumuman
+                  <i class="right fas fa-angle-left"></i>
+                </p>
+              </a>
+              <ul class="nav nav-treeview">
+                <li class="nav-item">
+                  <a href="/pengumuman" class="nav-link">
+                    <i class="far fa-circle nav-icon"></i>
+                    <p>Pengumuman AsDos</p>
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a href="pengumuman" class="nav-link">
+                    <i class="far fa-circle nav-icon"></i>
+                    <p>Pengumuman Lainnya</p>
+                  </a>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </nav>
+        <!-- /.sidebar-menu -->
+      </div>
+      <!-- /.sidebar -->
+    </aside>
+
     <!-- Main content -->
-    <div class="content">
+    <div class="content-wrapper">
       <div class="container">
         <?= $this->renderSection('content') ?>
-      </div><!-- /.container-fluid -->
+      </div>
     </div>
+    <!-- /.container-fluid -->
     <!-- /.content -->
   </div>
   <!-- /.content-wrapper -->
@@ -82,13 +113,12 @@ scratch. This page gets rid of all links and provides the needed markup only.
   </aside>
   <!-- /.control-sidebar -->
 
-
-  <!-- REQUIRED SCRIPTS -->
-
   <!-- jQuery -->
   <script src="/assets/AdminLTE/plugins/jquery/jquery.min.js"></script>
   <!-- Bootstrap 4 -->
   <script src="/assets/AdminLTE/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <!-- overlayScrollbars -->
+  <script src="/assets/AdminLTE/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
   <!-- AdminLTE App -->
   <script src="/assets/AdminLTE/dist/js/adminlte.min.js"></script>
   <!-- AdminLTE for demo purposes -->
