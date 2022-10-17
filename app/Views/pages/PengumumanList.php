ba0c9bc773fcd6a81a704021c29abfbbd7207d9f
Regita Rose Prameswari <83407800+Regita-99@users.noreply.github.com>
2022-10-18 01:12:46 +0700

    baru3

--- app/Views/pages/PengumumanList.php
+++ app/Views/pages/PengumumanList.php
@@ -14,8 +14,8 @@
 
                         <div class="p-4">
                             <a href="/store" type="button" class="btn btn-primary mb-3">Tambah</a>
-                            <table class="table table-striped">
-                                <thead>
+                            <!-- <table class="table table-striped"> -->
+                            <!-- <thead>
                                     <tr>
                                         <th scope="col">#</th>
                                         <th scope="col">Judul</th>
@@ -23,29 +23,45 @@
                                         <th scope="col">Created_At</th>
                                         <th scope="col">Action</th>
                                     </tr>
-                                </thead>
-                                <tbody>
-                                    <?php $no = 1; ?>
-                                    <?php foreach ($ini as $p) : ?>
+                                </thead> -->
+                            <!-- <tbody> -->
+
+
+                            <?php foreach ($ini as $p) : ?>
+                                <div class="card card-warning">
+                                    <div class="card-header">
+                                        <h3 class="card-title"></h3>
+                                    </div>
+                                    <table class="table table-sm">
                                         <tr>
-                                            <th scope="row"><?= $no ?></th>
-                                            <td><?= $p['judul'] ?></td>
-                                            <td><?= $p['deskripsi'] ?></td>
-                                            <td><?= $p['created_at'] ?></td>
+                                            <td>Title Of Announcement : <?= $p['judul'] ?></td>
+                                            <td align="right"><?= $p['created_at'] ?></td>
+                                        </tr>
+                                        <tr>
+                                            <td>Description :
+                                                <?= $p['deskripsi'] ?>
+                                            </td>
+                                            <td></td>
+                                        </tr>
+                                        <tr>
                                             <td>
                                                 <div class="d-flex">
                                                     <a class="btn btn-warning me-3" href="/edit/<?= $p['id'] ?>">Edit</a>
-                                                    <form action="/delete/<?= $p['id'] ?>" method="post">
+                                                    <form action="/delete/<?= $p['id'] ?>" method="post" Style="Margin-left: 5px">
                                                         <input type="hidden" name="_method" value="DELETE">
                                                         <button type="submit" class="btn btn-danger">Delete</button>
                                                     </form>
                                                 </div>
                                             </td>
+                                            <td></td>
                                         </tr>
-                                    <?php $no++;
-                                    endforeach ?>
-                                </tbody>
-                            </table>
+                                    </table>
+                                </div>
+                            <?php endforeach ?>
+
+                            <!-- </tbody> -->
+                            <!-- </table> -->
+                        </div>
                         </div>
 
                     </div>
