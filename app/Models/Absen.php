a05bf9d3849754428a92288cb77c87b669357fab
nesadwi <83526578+nesadwi@users.noreply.github.com>
2022-10-30 22:42:35 +0700

    Add files via upload

--- app/Models/Absen.php
+++ app/Models/Absen.php
@@ -9,9 +9,10 @@ class Absen extends Model
     protected $table            = 'absen';
     protected $primaryKey       = 'id';
     protected $useAutoIncrement = true;
-    protected $allowedFields    = ['nama_lengkap', 'npm_asdos', 'mata_kuliah', 'ruang', 'kelas_praktikan', 'hari', 'jam_masuk', 'kondisi_lab', 'created'];
+    protected $allowedFields    = ['nama_lengkap', 'npm_asdos', 'mata_kuliah', 'ruang', 'kelas_praktikan', 'hari', 'jam_masuk', 'kondisi_lab'];
 
-
-    protected $useTimestamps = true;
-    protected $dateFormat    = 'datetime';
+    public function getLastID()
+    {
+        return $this->db->query("select * from absen order by id desc")->getRow();
+    }
 }
