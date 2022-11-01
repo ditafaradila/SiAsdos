7c29f646a22212dd3f1bb7579c83d46f16478c10
Dita Faradila <74691609+ditafaradila@users.noreply.github.com>
2022-11-01 08:16:12 +0700

    Add files via upload

--- app/Views/welcome_message.php
+++ app/Views/welcome_message.php
@@ -1,35 +1,232 @@
 <!DOCTYPE html>
+<html lang="en">
+
 <head>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>SiAsdos</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="/assets/AdminLTE/dist/img/unila.jpg" rel="icon">
+  <link href="assetss/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Jost:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assetss/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assetss/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assetss/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assetss/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assetss/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assetss/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="assetss/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assetss/css/style.css" rel="stylesheet">
 </head>
+
 <body>
-    <div class="container">
-        <div class="alert alert-dark" role="alert">
-            Login Sebagai ...
+    <!-- ======= Header ======= -->
+    <header id="header" class="fixed-top ">
+        <div class="container d-flex align-items-center">
+            <h1 class="logo me-auto"><a href="index.html">SiAsdos</a></h1>
+            <nav id="navbar" class="navbar">
+                <ul>
+                    <li><a class="nav-link scrollto" href="#about">About</a></li>
+                    <li><a class="nav-link scrollto" href="#team">Team</a></li>
+                    <li><a class="nav-link scrollto" href="#login">Login</a></li>
+                </ul>
+            <i class="bi bi-list mobile-nav-toggle"></i>
+            </nav><!-- .navbar -->
         </div>
-        <div class="container px-4">
-            <div class="row gx-5">
-                <div class="col" align="center">
-                    <div class="p-3 border bg-light">
-                        <img src="/assets/AdminLTE/dist/img/student.jpg" class="rounded mx-auto d-block" height="150px">
-                        <a href="/mahasiswa" type="button" class="btn btn-secondary btn-sm">Mahasiswa</a>
+    </header>
+    <!-- End Header -->
+
+    <!-- ======= Hero Section ======= -->
+    <section id="hero" class="d-flex align-items-center">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 d-flex flex-column justify-content-center pt-4 pt-lg-0 order-2 order-lg-1" data-aos="fade-up" data-aos-delay="200">
+                <h1>Sistem Informasi Asisten Dosen</h1>
+                <h2>Mempermudah sistem informasi dalam dunia Asisten Dosen Jurusan Ilmu Komputer</h2>
+                <div class="d-flex justify-content-center justify-content-lg-start">
+                    <a href="#about" class="btn-get-started scrollto">Get Started</a>
+                    <a href="https://www.youtube.com/watch?v=jDDaplaOz7Q" class="glightbox btn-watch-video"><i class="bi bi-play-circle"></i><span>Watch Video</span></a>
+                </div>
+                </div>
+                <div class="col-lg-6 order-1 order-lg-2 hero-img" data-aos="zoom-in" data-aos-delay="200">
+                <img src="assetss/img/hero-img.png" class="img-fluid animated" alt="">
+                </div>
+            </div>
+        </div>
+    </section><!-- End Hero -->
+
+    <main id="main">
+        <!-- ======= About Us Section ======= -->
+        <section id="about" class="about">
+        <div class="container" data-aos="fade-up">
+            <div class="section-title">
+                <h2>About Us</h2>
+            </div>
+
+            <div class="row content">
+                <div class="col-lg-6">
+                    <p>
+                    Sistem Informasi Asisten Dosen merupakan sistem yang dapat membantu para mahasiswa serta Asisten Dosen dalam kegiatan belajar mengajar di kampus.
+                    </p>
+                    <ul>
+                        <li><i class="ri-check-double-line"></i> Mudah digunakan</li>
+                        <li><i class="ri-check-double-line"></i> Dapat diakses dimana saja</li>
+                        <li><i class="ri-check-double-line"></i> Tampilan yang menarik</li>
+                    </ul>
+                </div>
+                <div class="col-lg-6 pt-4 pt-lg-0">
+                    <p>
+                    Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+                    velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                    culpa qui officia deserunt mollit anim id est laborum.
+                    </p>
+                </div>
+            </div>
+
+        </div>
+        </section><!-- End About Us Section -->
+
+        <!-- ======= Team Section ======= -->
+        <section id="team" class="team section-bg">
+        <div class="container" data-aos="fade-up">
+            <div class="section-title">
+                <h2>Team</h2>
+                <p>Tim pengembang Sistem Informasi Asisten Dosen terdiri dari 4 wanita hebat</p>
+            </div>
+
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="member d-flex align-items-start" data-aos="zoom-in" data-aos-delay="100">
+                    <div class="pic"><img src="/assets/AdminLTE/dist/img/cihuy1.png" class="img-fluid" alt=""></div>
+                    <div class="member-info">
+                        <h4>Dita Faradila</h4>
+                        <span>2017051002</span>
+                        <p>Explicabo voluptatem mollitia et repellat qui dolorum quasi</p>
+                        <div class="social">
+                            <a href="https://www.instagram.com/dita_faa/"><i class="ri-instagram-fill"></i></a>
+                        </div>
+                    </div>
                     </div>
                 </div>
-                <div class="col" align="center">
-                    <div class="p-3 border bg-light">
-                        <img src="/assets/AdminLTE/dist/img/lecturer.jpg" class="rounded mx-auto d-block" height="150px">
-                        <a href="/asdos_login" type="button" class="btn btn-secondary btn-sm">Asisten Dosen</a>
+
+                <div class="col-lg-6 mt-4 mt-lg-0">
+                    <div class="member d-flex align-items-start" data-aos="zoom-in" data-aos-delay="200">
+                    <div class="pic"><img src="/assets/AdminLTE/dist/img/nesa2.jpeg" class="img-fluid" alt=""></div>
+                    <div class="member-info">
+                        <h4>Nesa Dwi Cahyani</h4>
+                        <span>2017051009</span>
+                        <p>Aut maiores voluptates amet et quis praesentium qui senda para</p>
+                        <div class="social">
+                            <a href="https://www.instagram.com/nesadwi/"><i class="ri-instagram-fill"></i></a>
+                        </div>
+                    </div>
                     </div>
                 </div>
-                <div class="col" align="center">
-                    <div class="p-3 border bg-light">
-                        <img src="/assets/AdminLTE/dist/img/admin.jpg" class="rounded mx-auto d-block" height="150px">
-                        <a href="/admin_login" type="button" class="btn btn-secondary btn-sm"> Admin </a>
+
+                <div class="col-lg-6 mt-4">
+                    <div class="member d-flex align-items-start" data-aos="zoom-in" data-aos-delay="300">
+                    <div class="pic"><img src="/assets/AdminLTE/dist/img/sarah.jpg" class="img-fluid" alt=""></div>
+                    <div class="member-info">
+                        <h4>Sarah Cantika Putri</h4>
+                        <span>2017051083</span>
+                        <p>Quisquam facilis cum velit laborum corrupti fuga rerum quia</p>
+                        <div class="social">
+                        <a href="https://www.instagram.com/saraahcntka_/"><i class="ri-instagram-fill"></i></a>
+                    </div>
+                    </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-6 mt-4">
+                    <div class="member d-flex align-items-start" data-aos="zoom-in" data-aos-delay="400">
+                    <div class="pic"><img src="/assets/AdminLTE/dist/img/regita.jpg" class="img-fluid" alt=""></div>
+                    <div class="member-info">
+                        <h4>Regita Rose Prameswari</h4>
+                        <span>2017051017</span>
+                        <p>Dolorum tempora officiis odit laborum officiis et et accusamus</p>
+                        <div class="social">
+                        <a href="https://www.instagram.com/regitarpm/"><i class="ri-instagram-fill"></i></a>
+                        </div>
+                    </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
+        </section><!-- End Team Section -->
+
+        <!-- ======= Pricing Section ======= -->
+        <section id="login" class="pricing">
+        <div class="container">
+            <div class="section-title">
+                <h2>Login</h2>
+            </div>
+
+            <div class="container px-4">
+                <div class="row gx-5">
+                    <div class="col" align="center">
+                        <div class="p-3 border bg-light">
+                            <img src="/assets/AdminLTE/dist/img/student.jpg" class="rounded mx-auto d-block" height="150px">
+                            <a href="/mhs_login" type="button" class="btn btn-primary btn-sm">Mahasiswa</a>
+                        </div>
+                    </div>
+                    <div class="col" align="center">
+                        <div class="p-3 border bg-light">
+                            <img src="/assets/AdminLTE/dist/img/lecturer.jpg" class="rounded mx-auto d-block" height="150px">
+                            <a href="/asdos_login" type="button" class="btn btn-primary btn-sm">Asisten Dosen</a>
+                        </div>
+                    </div>
+                    <div class="col" align="center">
+                        <div class="p-3 border bg-light">
+                            <img src="/assets/AdminLTE/dist/img/admin.jpg" class="rounded mx-auto d-block" height="150px">
+                            <a href="/admin_login" type="button" class="btn btn-primary btn-sm"> Admin </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </section><!-- End Pricing Section -->
+
+    <!-- ======= Footer ======= -->
+    <footer id="footer">
+        <div class="container footer-bottom clearfix">
+        <div class="copyright">
+            &copy; Copyright <strong><span>SiAsdos</span></strong>. All Rights Reserved
+        </div>
+        <div class="credits">
+            <!-- All the links in the footer should remain intact. -->
+            <!-- You can delete the links only if you purchased the pro version. -->
+            <!-- Licensing information: https://bootstrapmade.com/license/ -->
+            <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/arsha-free-bootstrap-html-template-corporate/ -->
+            Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+        </div>
+        </div>
+    </footer><!-- End Footer -->
+
+    <div id="preloader"></div>
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+    <!-- Vendor JS Files -->
+    <script src="assetss/vendor/aos/aos.js"></script>
+    <script src="assetss/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assetss/vendor/glightbox/js/glightbox.min.js"></script>
+    <script src="assetss/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+    <script src="assetss/vendor/swiper/swiper-bundle.min.js"></script>
+    <script src="assetss/vendor/waypoints/noframework.waypoints.js"></script>
+    <script src="assetss/vendor/php-email-form/validate.js"></script>
+
+    <!-- Template Main JS File -->
+    <script src="assetss/js/main.js"></script>
+
 </body>
+
 </html>
