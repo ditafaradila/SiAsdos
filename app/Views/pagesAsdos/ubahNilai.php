e98fad01bcd3faaf218a36830679912a9a1d4fb4
Regita Rose Prameswari <83407800+Regita-99@users.noreply.github.com>
2022-11-01 08:07:15 +0700

    Update ubahNilai.php

--- app/Views/pagesAsdos/ubahNilai.php
+++ app/Views/pagesAsdos/ubahNilai.php
@@ -14,34 +14,31 @@ use Kint\Zval\Value;
         <!-- form start -->
         <form  action="/ubah/<?=$value['id']?> ?>" method="post">
             <div class="card-body">
+                <div class="card-body">
                 <div class="form-group">
                     <label>Kode MK</label>
-                    <input type="text" class="form-control"  name="mk" id="mk" value="<?= $value['mk'] ?>>
+                    <input type="text" class="form-control"  name="mk" id="mk" value="<?= $value['mk']?>">
                 </div>
                 <div class="form-group">
                     <label>Kurikulum</label>
-                    <input type="text" class="form-control"  name="kurikulum" id="kurikulum" value="<?= $value['kurikulum'] ?>>
+                    <input type="text" class="form-control"  name="kurikulum" id="kurikulum" value="<?= $value['kurikulum']?>">
                 </div>
                 <div class="form-group">
                     <label>Nama</label>
-                    <input type="text" class="form-control"  name="nama" id="nama" value="<?= $value['nama'] ?>>
+                    <input type="text" class="form-control"  name="nama" id="nama" value="<?= $value['nama'] ?>">
                 </div>
                 <div class="form-group">
                     <label>NPM</label>
-                    <input type="text" class="form-control" name="npm" id="npm" value="<?= $value['npm'] ?>>
+                    <input type="text" class="form-control" name="npm" id="npm" value="<?= $value['npm'] ?>">
                 </div>
                 <div class="form-group">
                     <label>Kelas</label>
-                    <input type="text" class="form-control" name="kelas" id="kelas" value="<?= $value['kelas'] ?>>
-                        <!-- <option>A</option>
-                        <option>B</option>
-                        <option>C</option>
-                        <option>D</option> -->
-                    <!-- </select> -->
+                    <input type="text" class="form-control" name="kelas" id="kelas" value="<?= $value['kelas'] ?>">
+                        
                 </div>
                 <div class="form-group">
                     <label>Nilai</label>
-                    <input type="text" class="form-control" name="nilai" id="nilai" value="<?= $value['nilai'] ?>>
+                    <input type="text" class="form-control" name="nilai" id="nilai" value="<?= $value['nilai'] ?>">
                 </div>
             </div>
             <!-- /.card-body -->
