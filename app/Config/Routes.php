9b331271faa4e513b5939d6672497f8da2a9e2da
nesadwi <83526578+nesadwi@users.noreply.github.com>
2022-11-06 17:44:32 +0700

    nesadwi

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -38,6 +38,7 @@ $routes->set404Override();
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
 
+//nama pages -> nama function
 $routes->get('/admin_login', 'LoginAdmin::index');
 $routes->post('/login/process', 'LoginAdmin::process');
 $routes->get('/logout', 'LoginAdmin::logout');
@@ -49,47 +50,14 @@ $routes->get('/logout', 'LoginAsdos::logout');
 $routes->get('/mhs_login', 'MhsLogin::index');
 $routes->post('/login/proces', 'MhsLogin::proces');
 $routes->get('/logout', 'MhsLogin::logout');
-$routes->get('/PMmateri', 'MhsLogin::indexx');
+//$routes->get('/PMmateri', 'MhsLogin::indexx');
 
 $routes->get('/Ashome', 'AsdosController::index');
 
 $routes->get('/profile', 'Profile::index');
 $routes->get('/editProfile', 'Profile::update');
 
-$routes->get('/materi', 'MateriController::mahasiswa');
-
-$routes->get('/profile', 'Profile::index');
-$routes->post('/create', 'PengumumanControl::create');
-$routes->get('/store', 'PengumumanControl::store');
-$routes->get('/pages', 'PengumumanControl::index');
-$routes->get('/edit/(:num)', 'PengumumanControl::edit/$1');
-$routes->post('/update/(:num)', 'PengumumanControl::update/$1');
-$routes->delete('/delete/(:num)', 'PengumumanControl::delete/$1');
-$routes->get('/PengumumanLain', 'PengumumanControl::PengumumanLain');
-$routes->get('/', 'Home::index');
-$routes->get('/home', 'JadwalController::index');
-$routes->get('/edit/(:num)', 'JadwalController::edit/$1');
-$routes->post('/update/(:num)', 'JadwalController::update/$1');
-$routes->get('/jadwalPiket', 'JadwalPiketController::index');
-$routes->get('/create', 'JadwalPiketController::create');
-$routes->post('/store', 'JadwalPiketController::store');
-$routes->get('/edit/(:num)', 'JadwalPiketController::edit/$1');
-$routes->post('/update/(:num)', 'JadwalPiketController::update/$1');
-$routes->delete('/delete/(:num)', 'JadwalPiketController::delete/$1');
-
-$routes->get('pengumuman', 'PengumumanController::index');
-$routes->get('/createP', 'PengumumanController::createP');
-$routes->post('/storeP', 'PengumumanController::storeP');
-$routes->get('editP/(:num)', 'PengumumanController::editP/$1');
-$routes->post('updateP/(:num)', 'PengumumanController::updateP/$1');
-$routes->get('deleteP/(:num)', 'PengumumanController::deleteP/$1');
-
-$routes->get('/absen', 'AbsenController::index');
-$routes->get('/absenSelesai', 'AbsenController::absenSelesai');
-$routes->get('/CAbsen', 'AbsenController::createAbsen');
-$routes->post('/simpanAbsen', 'AbsenController::simpanAbsen');
-$routes->get('/EAbsen/(:num)', 'AbsenController::editAbsen/$1');
-$routes->post('/updateAbsen/(:num)', 'AbsenController::updateAbsen/$1');
+$routes->get('/PMmateri', 'MateriController::mahasiswa');
 
 $routes->get('/Nilai', 'Nilaicontrol::Nilai');
 $routes->post('/buat', 'Nilaicontrol::buat');
@@ -101,12 +69,58 @@ $routes->delete('/deleteNilai/(:num)', 'NilaiControl::delete/$1');
 $routes->get('/PengumumanLain', 'Nilaicontrol::PengumumanLain');
 $routes->get('/', 'Home::index');
 
+//PengumumanControl
+$routes->post('/create', 'PengumumanControl::create');
+$routes->get('/store', 'PengumumanControl::store');
+$routes->get('/pages', 'PengumumanControl::index');
+$routes->get('/edit/(:num)', 'PengumumanControl::edit/$1');
+$routes->post('/update/(:num)', 'PengumumanControl::update/$1');
+$routes->delete('/delete/(:num)', 'PengumumanControl::delete/$1');
+$routes->get('/PengumumanLain', 'PengumumanControl::PengumumanLain');
+
+//JadwalController
+$routes->get('/', 'Home::index');
+$routes->get('/home', 'JadwalController::index');
+$routes->get('/edit/(:num)', 'JadwalController::edit/$1');
+$routes->post('/update/(:num)', 'JadwalController::update/$1');
+$routes->get('/jadwalPiket', 'JadwalPiketController::index');
+$routes->get('/create', 'JadwalPiketController::create');
+$routes->post('/store', 'JadwalPiketController::store');
+$routes->get('/edit/(:num)', 'JadwalPiketController::edit/$1');
+$routes->post('/update/(:num)', 'JadwalPiketController::update/$1');
+$routes->delete('/delete/(:num)', 'JadwalPiketController::delete/$1');
+
+//pengumuman asdos
+$routes->get('pengumuman', 'PengumumanController::index');
+$routes->get('/createP', 'PengumumanController::createP');
+$routes->post('/storeP', 'PengumumanController::storeP');
+$routes->get('editP/(:num)', 'PengumumanController::editP/$1');
+$routes->post('updateP/(:num)', 'PengumumanController::updateP/$1');
+$routes->get('deleteP/(:num)', 'PengumumanController::deleteP/$1');
+
+//absen Asdos
+$routes->get('/absen', 'AbsenController::index');
+$routes->get('/absenSelesai', 'AbsenController::absenSelesai');
+$routes->get('/CAbsen', 'AbsenController::createAbsen');
+$routes->post('/simpanAbsen', 'AbsenController::simpanAbsen');
+$routes->get('/EAbsen/(:num)', 'AbsenController::editAbsen/$1');
+$routes->post('/updateAbsen/(:num)', 'AbsenController::updateAbsen/$1');
+
+//absen Mahasiswa
+$routes->get('/absenMhs', 'AbsenMhsController::index');
+$routes->get('/absenSelesaiMhs', 'AbsenMhsController::absenSelesaiMhs');
+$routes->get('/CAbsenMhs', 'AbsenMhsController::createAbsenMhs');
+$routes->post('/simpanAbsenMhs', 'AbsenMhsController::simpanAbsenMhs');
+$routes->get('/EAbsenMhs/(:num)', 'AbsenMhsController::editAbsenMhs/$1');
+$routes->post('/updateAbsenMhs/(:num)', 'AbsenMhsController::updateAbsenMhs/$1');
+
 $routes->get('/materi', 'MateriController::index');
 $routes->get('/Cmateri', 'MateriController::Create');
 $routes->post('/Store', 'MateriController::Store');
 $routes->get('/Emateri/(:num)', 'MateriController::Edit/$1');
 $routes->post('/Update/(:num)', 'MateriController::Update/$1');
 $routes->delete('/Delete/(:num)', 'MateriController::Delete/$1');
+$routes->post('/Download/(:num)', 'MateriController::Download/$1');
 
 $routes->get('/(:any)', 'Pages::view/$1');
 $routes->get('/(:any)', 'PagesAsdos::view/$1');
