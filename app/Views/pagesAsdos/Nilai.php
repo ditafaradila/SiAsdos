f05fcf6a1fc7c864f2607acfdfbdab8086eaeb42
Regita Rose Prameswari <83407800+Regita-99@users.noreply.github.com>
2022-11-08 09:47:27 +0700

    Update Nilai.php

--- app/Views/pagesAsdos/Nilai.php
+++ app/Views/pagesAsdos/Nilai.php
@@ -8,15 +8,23 @@
         </div>
         <!-- /.card-header -->
         <!-- form start -->
-        <form  action="<?= base_url('/buat') ?>" method="post">
+        <form action="<?= base_url('/buat') ?>" method="post">
             <div class="card-body">
                 <div class="form-group">
                     <label>Kode MK</label>
-                    <input type="text" class="form-control" placeholder="Masukkan Nama" name="mk" id="mk">
+                    <select name="mk" id="mk" class="form-control">
+                        <?php foreach ($values as $kp) : ?>
+                            <option value="<?= $kp['mk'] ?>"><?= $kp['mk'] ?></option>
+                        <?php endforeach; ?>
+                    </select>
                 </div>
                 <div class="form-group">
                     <label>Kurikulum</label>
-                    <input type="text" class="form-control" placeholder="Masukkan Kurikulum" name="kurikulum" id="kurikulum">
+                    <select name="kurikulum" id="kurikulum" class="form-control">
+                        <?php foreach ($p as $kp) : ?>
+                            <option value="<?= $kp['kurikulum'] ?>"><?= $kp['kurikulum'] ?></option>
+                        <?php endforeach; ?>
+                    </select>
                 </div>
                 <div class="form-group">
                     <label>Nama</label>
@@ -28,13 +36,11 @@
                 </div>
                 <div class="form-group">
                     <label>Kelas</label>
-                    <input type="text" class="form-control" placeholder="Masukkan Kelas" name="kelas" id="kelas">
-                    <!-- <select class="form-control select2" style="width: 100%;">
-                        <option>A</option>
-                        <option>B</option>
-                        <option>C</option>
-                        <option>D</option>
-                    </select> -->
+                    <select name="kelas" id="kelas" class="form-control">
+                        <?php foreach ($kls as $kp) : ?>
+                            <option value="<?= $kp['kelas'] ?>"><?= $kp['kelas'] ?></option>
+                        <?php endforeach; ?>
+                    </select>
                 </div>
                 <div class="form-group">
                     <label>Nilai</label>
