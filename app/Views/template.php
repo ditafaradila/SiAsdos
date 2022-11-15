d7fb3f71615462b374a31b5ad9efb5cebb8405d1
nesadwi <83526578+nesadwi@users.noreply.github.com>
2022-11-15 19:56:13 +0700

    Add files via upload

--- app/Views/template.php
+++ app/Views/template.php
@@ -2,171 +2,145 @@
 <html lang="en">
 
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Sistem Informasi Asdos</title>
 
-  <!-- Google Font: Source Sans Pro -->
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-  <!-- Font Awesome -->
-  <link rel="stylesheet" href="/assets/AdminLTE/plugins/fontawesome-free/css/all.min.css">
-  <!-- overlayScrollbars -->
-  <link rel="stylesheet" href="/assets/AdminLTE/plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
-  <!-- Theme style -->
-  <link rel="stylesheet" href="/assets/AdminLTE/dist/css/adminlte.min.css">
-  <link rel="stylesheet" href="/assets/AdminLTE/plugins/summernote/summernote-bs4.min.css">
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>ADMIN</title>
+
+  <!-- Custom fonts for this template-->
+  <link href="/assetsadmin/adminstart/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+  <!-- Custom styles for this template-->
+  <link href="/assetsadmin/adminstart/css/sb-admin-2.min.css" rel="stylesheet">
+
 </head>
 
-<body class="hold-transition sidebar-mini layout-fixed">
-  <!-- Site wrapper -->
-  <div class="wrapper">
-    <!-- Navbar -->
-    <nav class="main-header navbar navbar-expand navbar-white navbar-light bg-info">
-      <div class="container">
-        <!-- Left navbar links -->
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
-          </li>
-        </ul>
-      </div>
-    </nav>
-    <!-- /.navbar -->
+<body id="page-top">
 
-    <!-- Main Sidebar Container -->
-    <aside class="main-sidebar sidebar-dark-primary elevation-4 bg-info">
-      <!-- Brand Logo -->
-      <a href="#" class="brand-link">
-        <img src="/assets/AdminLTE/dist/img/unila.jpg" alt="Person Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-        <span class="brand-text font-weight-light">SISTEM INFORMASI</span>
+  <!-- Page Wrapper -->
+  <div id="wrapper">
+
+    <!-- Sidebar -->
+    <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+      <!-- Sidebar - Brand -->
+      <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+        <div class="sidebar-brand-icon rotate-n-15">
+          <i class="fas fa-laugh-wink"></i>
+        </div>
+        <div class="sidebar-brand-text mx-3">Admin ASDOS</div>
       </a>
 
-      <!-- Sidebar -->
-      <div class="sidebar">
-        <!-- Sidebar user (optional) -->
-        <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-          <div class="image">
-            <img src="/assets/AdminLTE/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
-          </div>
-          <div class="info">
-            <a href="#" class="d-block"><?= session()->get('nama'); ?></a>
+      <!-- Divider -->
+      <!-- <hr class="sidebar-divider my-0"> -->
+
+      <!-- Divider -->
+      <hr class="sidebar-divider">
+
+      <!-- Heading -->
+      <div class="sidebar-heading">
+
+      </div>
+
+      <!-- Nav Item - Pages Collapse Menu -->
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+          <i class="fas fa-calendar"></i>
+          <span>Jadwal</span>
+        </a>
+        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+          <div class="bg-white py-2 collapse-inner rounded">
+            <h6 class="collapse-header">Jadwal</h6>
+            <a class="collapse-item" href="/home">Jadwal Asdos</a>
+            <a class="collapse-item" href="/jadwalPiket">Jadwal Piket</a>
           </div>
         </div>
+      </li>
 
-        <!-- Sidebar Menu -->
-        <nav class="mt-2">
-          <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-            <!-- Add icons to the links using the .nav-icon class
-               with font-awesome or any other icon font library -->
-            <li class="nav-item">
-              <a href="#" class="nav-link">
-                <i class="nav-icon fas fa-calendar"></i>
-                <p>
-                  Jadwal
-                  <i class="right fas fa-angle-left"></i>
-                </p>
-              </a>
-              <ul class="nav nav-treeview">
-                <li class="nav-item">
-                  <a href="/home" class="nav-link">
-                    <i class="nav-icon"></i>
-                    <p>Jadwal AsDos</p>
-                  </a>
-                </li>
-                <li class="nav-item">
-                  <a href="/jadwalPiket" class="nav-link">
-                    <i class="nav-icon"></i>
-                    <p>Jadwal Piket</p>
-                  </a>
-                </li>
-              </ul>
-            </li>
-            <li class="nav-item">
-              <a href="#" class="nav-link">
-                <i class="nav-icon fas fa-bell"></i>
-                <p>
-                  Pengumuman
-                  <i class="right fas fa-angle-left"></i>
-                </p>
-              </a>
-              <ul class="nav nav-treeview">
-                <li class="nav-item">
-                  <a href="/pengumuman" class="nav-link">
-                    <i class="nav-icon"></i>
-                    <p>Pengumuman AsDos</p>
-                  </a>
-                </li>
-                <li class="nav-item">
-                  <a href="/pengumumanLain" class="nav-link">
-                    <i class="nav-icon"></i>
-                    <p>Pengumuman Lainnya</p>
-                  </a>
-                </li>
-              </ul>
-            </li>
-            <li class="nav-item">
-              <a href="#" class="nav-link">
-                <i class="nav-icon fas fa-list-alt"></i>
-                <p>
-                  Rekapan Absensi
-                  <i class="right fas fa-angle-left"></i>
-                </p>
-              </a>
-              <ul class="nav nav-treeview">
-                <li class="nav-item">
-                  <a href="/absen" class="nav-link">
-                    <i class="nav-icon"></i>
-                    <p>Absen Asdos</p>
-                  </a>
-                </li>
-                <li class="nav-item">
-                  <a href="/absenMhs" class="nav-link">
-                    <i class="nav-icon"></i>
-                    <p>Absen Mahasiswa</p>
-                  </a>
-                </li>
-              </ul>
-            </li>
-          </ul>
-        </nav>
-        <!-- /.sidebar-menu -->
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+          <i class="fas fa-bell"></i>
+          <span>Pengumuman</span>
+        </a>
+        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+          <div class="bg-white py-2 collapse-inner rounded">
+            <h6 class="collapse-header">Pengumuman</h6>
+            <a class="collapse-item" href="/pengumuman">Pengumuman Asdos</a>
+            <a class="collapse-item" href="/pengumumanLain">Pengumuman Lainnya</a>
+          </div>
+        </div>
+      </li>
+
+
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+          <i class="fas fa-list-alt"></i>
+          <span>Absen</span>
+        </a>
+        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+          <div class="bg-white py-2 collapse-inner rounded">
+            <h6 class="collapse-header">Rekapan Absen</h6>
+            <a class="collapse-item" href="/absen">Absen Asdos</a>
+            <a class="collapse-item" href="/absenMhs">Absen Mahasiswa</a>
+          </div>
+        </div>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="/pendaftarAsdos">
+          <i class="fas fa-book"></i>
+          <span>Pendaftaran Asdos</span>
+        </a>
+      </li>
+      <!-- Nav Item - Utilities Collapse Menu -->
+
+
+      <!-- Divider -->
+      <hr class="sidebar-divider d-none d-md-block">
+
+      <!-- Sidebar Toggler (Sidebar) -->
+      <div class="text-center d-none d-md-inline">
+        <button class="rounded-circle border-0" id="sidebarToggle"></button>
       </div>
-      <!-- /.sidebar -->
-    </aside>
 
-    <!-- Main content -->
+
+    </ul>
+    <!-- End of Sidebar -->
+
+
+    <!-- Main Content -->
     <div class="content-wrapper">
       <div class="container">
         <?= $this->renderSection('content') ?>
       </div>
     </div>
-    <!-- /.container-fluid -->
-    <!-- /.content -->
-  </div>
-  <!-- /.content-wrapper -->
 
-  <!-- Control Sidebar -->
-  <aside class="control-sidebar control-sidebar-dark">
-    <!-- Control sidebar content goes here -->
-  </aside>
-  <!-- /.control-sidebar -->
+    <!-- Bootstrap core JavaScript-->
+    <script src="/assetsadmin/adminstart/vendor/jquery/jquery.min.js"></script>
+    <script src="/assetsadmin/adminstart/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
-  <!-- jQuery -->
-  <script src="/assets/AdminLTE/plugins/jquery/jquery.min.js"></script>
-  <!-- Bootstrap 4 -->
-  <script src="/assets/AdminLTE/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <!-- overlayScrollbars -->
-  <script src="/assets/AdminLTE/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
-  <!-- AdminLTE App -->
-  <script src="/assets/AdminLTE/dist/js/adminlte.min.js"></script>
-  <!-- AdminLTE for demo purposes -->
-  <script src="/assets/AdminLTE/dist/js/demo.js"></script>
-  <script src="/assets/AdminLTE/plugins/summernote/summernote-bs4.min.js"></script>
-  <script>
-    $(function() {
-      $('#deskripsi').summernote()
-    })
-  </script>
+    <!-- Core plugin JavaScript-->
+    <script src="/assetsadmin/adminstart/vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="/assetsadmin/adminstart/js/sb-admin-2.min.js"></script>
+
+    <!-- Page level plugins -->
+    <script src="/assetsadmin/adminstart/vendor/chart.js/Chart.min.js"></script>
+
+    <!-- Page level custom scripts -->
+    <script src="/assetsadmin/adminstart/js/demo/chart-area-demo.js"></script>
+    <script src="/assetsadmin/adminstart/js/demo/chart-pie-demo.js"></script>
+    <script>
+      $(function() {
+        $('#deskripsi').summernote()
+      })
+    </script>
 </body>
 
 </html>
