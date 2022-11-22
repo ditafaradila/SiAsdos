22a4729c3a697af4540eee1ecbf59e7c7579b77b
Regita Rose Prameswari <83407800+Regita-99@users.noreply.github.com>
2022-11-22 16:45:00 +0700

    minggu lalu Pengumuman change

--- app/Views/pages/pengumumanLain.php
+++ app/Views/pages/pengumumanLain.php
@@ -2,48 +2,29 @@
 <?= $this->section('content') ?>
 
 <!-- Main content -->
-<div class="mt-3">
-    <section class="content">
-        <div class="container-fluid">
-            <div class="row">
-                <!-- left column -->
-                <div class="col-md-12">
-                    <!-- jquery validation -->
-                    <div class="card card-warning">
-                        <div class="card-header">
-                            <h3 class="card-title"></h3>
-                        </div>
-                        <!-- /.card-header -->
-                        <!-- form start -->
-                        <form action="<?= base_url('/create') ?>" method="post">
-                            <div class="card-body">
-                                <div class="form-group">
-                                    <label>Title Anouncement</label>
-                                    <input type="text" class="form-control is-valid" placeholder="Enter ..." name="judul" id="judul" class="form-control">
-                                </div>
-                                <div class="form-group">
-                                    <label for="deskripsi" class="form-label">Content of Anouncement</label>
-                                    <textarea name="deskripsi" class="form-control" id="deskripsi"></textarea>
-                                </div>
-                            </div>
-                            <!-- /.card-body -->
-                            <div class="card-footer">
-                                <button type="submit" class="btn btn-primary">Submit</button>
-                            </div>
-                        </form>
-                    </div>
-                    <!-- /.card -->
+<div class="md-6 mt-3">
+    <!-- general form elements -->
+    <div class="card card-primary">
+    <div class="card-header bg-primary text-white">
+            <center> Pengumuman Asisten Dosen </center>
+        </div>
+        <form id="quickForm" action="http://localhost:8080/create" method="post">
+            <div class="card-body ">
+                <div class="form-group">
+                    <label>Title Anouncement</label>
+                    <input type="text" class="form-control is-valid" placeholder="Enter ..." name="judul" id="judul">
                 </div>
-                <!--/.col (left) -->
-                <!-- right column -->
-                <div class="col-md-6">
-
+                <div class="form-group">
+                    <label for="deskripsi" class="form-label">Content of Anouncement</label>
+                    <textarea name="deskripsi" class="form-control" id="deskripsi"></textarea>
                 </div>
-                <!--/.col (right) -->
             </div>
-            <!-- /.row -->
-        </div><!-- /.container-fluid -->
-    </section>
+            <!-- /.card-body -->
+            <div class="card-footer">
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </div>
+        </form>
+    </div>
 </div>
-
+</div>
 <?= $this->endSection() ?>
